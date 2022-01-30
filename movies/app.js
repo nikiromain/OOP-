114f5905f9adfc9d6d663660bef2ed3677dcfdbf
Nikisha Romain <nikisharomain@Nikishas-MacBook-Air.local>
2022-01-29 23:33:17 -0400

    first commit

---
+++ movies/app.js
@@ -0,0 +1,26 @@
+const Movies = require ("./js/movies.js")
+
+
+const main = (()=>
+{
+
+    new movies = []
+
+    movies.push = new Movies ("SpiderMan", 120, "PG-13")
+    movies.push = new Movies ("Avengers", 320, "R")
+    movies.push = new Movies ("Thor", 150, "PG-13")
+
+    console.log(movies[movies.length-1].title)
+
+    const searchMovie = (arr, searchValue)=>
+    {
+        arr.find((arr)=>
+        {
+            if(movies===searchValue)
+            {
+                
+            }
+        })
+    }
+
+})()
---
+++ movies/js/movie.js
@@ -0,0 +1,18 @@
+const Movie 
+{
+    title; 
+    duration; 
+    rating; 
+
+
+    constructor (t, d, r)
+    {
+        this.title = t;
+        this.duration = d;
+        this.rating = r;
+    }
+
+
+}
+
+module.exports = Movie
